ee001f6a756dedf29ef84a0a0d821d9365ab8bd3
CasperGamingOne <caspergamingone@outlook.dk>
2026-06-29 13:45:44 +0300

    Progres la Machine.cs + Creare MachinePart.cs

--- Machine.cs
+++ Machine.cs
@@ -1,6 +1,32 @@
 ﻿namespace Smart_Factory_Management_System
 {
-    internal class Machine
+    public enum MachineStatus { Running, Stopped, Maintenance }
+
+    public enum MachineCondition { Excellent, Good, Critical }
+
+    abstract internal class Machine
     {
+        private protected string? machine_name, machine_manufacturer, machine_serial;
+
+        private protected MachineStatus status;
+
+        private protected MachineCondition condition;
+
+        public string? Name { get { return machine_name; } set { machine_name = value; } }
+
+        public string? Manufacturer { get { return machine_manufacturer; } set { machine_manufacturer = value; } }
+
+        public string? SerialNumber { get { return machine_serial; } set { machine_serial = value; } }
+
+        public MachineStatus Status { get { return status; } set { status = MachineStatus.Stopped; }  }
+
+        public MachineCondition Condition { get { return condition; } set { condition = MachineCondition.Excellent; } }
+
+        public Machine(string machine_name, string machine_manufacturer, string machine_serial)
+        {
+            Name = machine_name;
+            Manufacturer = machine_manufacturer;
+            SerialNumber = machine_serial;
+        }
     }
 }
---
+++ MachinePart.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Smart_Factory_Management_System
+{
+    internal class MachinePart
+    {
+        private protected string? part_name;
+
+        private protected bool? condition;
+
+        public string? Name { get { return part_name; } set { part_name = value; } }
+        public bool? Condition { get { return condition; } set { condition = true; } }
+
+        public MachinePart(string name)
+        {
+            Name = name; 
+        }
+    }
+}
