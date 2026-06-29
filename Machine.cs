02ad5195fb2f6c35a7cb1ea42cc2d8fb3ad5b5c7
CasperGamingOne <caspergamingone@outlook.dk>
2026-06-29 14:45:50 +0300

    implemented issue on warnings on github actions

--- Machine.cs
+++ Machine.cs
@@ -8,6 +8,10 @@
     {
         private protected string? machine_name, machine_manufacturer, machine_serial;
 
+        private protected DateTime installation_date;
+
+        private protected MachinePart[]? machine_parts;
+
         private protected MachineStatus status;
 
         private protected MachineCondition condition;
@@ -18,15 +22,23 @@
 
         public string? SerialNumber { get { return machine_serial; } set { machine_serial = value; } }
 
+        public DateTime InstallationDate { get { return installation_date; } set { installation_date = value; } }
+
+        public MachinePart[]? Parts { get { return machine_parts; } set { machine_parts = value; } }
+
         public MachineStatus Status { get { return status; } set { status = MachineStatus.Stopped; }  }
 
         public MachineCondition Condition { get { return condition; } set { condition = MachineCondition.Excellent; } }
 
-        public Machine(string machine_name, string machine_manufacturer, string machine_serial)
+        public Machine(string machine_name, string machine_manufacturer, string machine_serial, MachinePart[] parts)
         {
             Name = machine_name;
             Manufacturer = machine_manufacturer;
             SerialNumber = machine_serial;
+            InstallationDate = DateTime.Now.AddYears(-5);
+            Parts = parts;
         }
+
+        
     }
 }
